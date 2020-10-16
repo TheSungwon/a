df86263c57461a675612bd3cfdabdc57a231393f
TheSungwon <69591409+TheSungwon@users.noreply.github.com>
2020-10-16 18:20:04 +0900

    Add files via upload

---
+++ App/javaSE/bin/javaSE1015/javaSE1016/increaseDecreseOperatorExaple.java
@@ -0,0 +1,17 @@
+package javaSE1016;
+
+public class increaseDecreseOperatorExaple {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		int z;
+		
+		
+		System.out.println("-----");
+		x++;
+		++x;
+		System.out.println("x=" + x);
+		
+	}
+
+}
