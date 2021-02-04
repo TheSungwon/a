9dd96c0c66ed22d31fd7fa1c7943db1d171bdfd7
TheSungwon <69591409+TheSungwon@users.noreply.github.com>
2021-02-04 20:29:29 +0900

    academy 04 02 2021 spring4.0 sts chap07 springMVC1
    academy 04 02 2021 spring4.0 sts chap07 springMVC

---
+++ springMVC/src/main/java/spring4/mvc/chap7/quickStart/HelloController.java
@@ -0,0 +1,16 @@
+package spring4.mvc.chap7.quickStart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello.do")
+	public String hell(Model model) {
+		model.addAttribute("gre","¤¾¤·");
+		return "hello";
+	}
+
+}
